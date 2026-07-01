b4bfe9b0129b2cc1364f8d97c1cc4af24b4b4353
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-07-01 10:46:54 +0530

    Time: 7 ms (91.5%), Space: 19.4 MB (45.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        f1={}
+        f2={}
+        for i in s:
+            f1[i]=f1.get(i,0)+1
+        for i in t:
+            f2[i]=f2.get(i,0)+1
+        if f1==f2:
+            return True
+        return False    
+        
