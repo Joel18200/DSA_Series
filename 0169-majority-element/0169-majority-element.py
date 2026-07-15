20b7684b38b4e47c804e0e5a452ac4ab2c4a37df
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-07-15 20:09:40 +0530

    Time: 3 ms (85.23%), Space: 21.1 MB (85.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq={}
-        for i in nums:
-            freq[i]=freq.get(i,0)+1
-        return max(freq,key=freq.get)    
-            
+        holder=None
+        count=0
+        for num in nums:
+            if count==0:
+                holder=num
+            if holder==num:
+                count+=1
+            else:
+                count-=1
+        return holder                
