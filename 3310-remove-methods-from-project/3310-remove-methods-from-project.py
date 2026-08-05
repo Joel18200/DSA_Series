99882dd7b78aa22eff65c978aec742a05fd8c0b1
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-08-05 10:49:14 +0530

    Time: 258 ms (78.79%), Space: 144.5 MB (36.36%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.py
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -17,7 +17,7 @@ class Solution:
         '''
         for caller,callee in invocations:
             if not suspicous[caller] and suspicous[callee]:
-                return list(range(n))
+                return [i for i in range(n)]
         '''
         if there are suspicous methods and there are no outside methods connected to it
         we can remove the suspicous methods
