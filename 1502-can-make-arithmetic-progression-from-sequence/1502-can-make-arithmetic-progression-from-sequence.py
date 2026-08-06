69650f38555b3ef81015b5678d6a637538be2883
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-08-06 19:07:26 +0530

    Time: 2 ms (24.5%), Space: 19.3 MB (34.07%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -3,7 +3,7 @@ class Solution:
         arr.sort()
         s=[]
         for num in range(len(arr)-1):
-            i=abs(arr[num]-arr[num+1])
+            i=arr[num]-arr[num+1]
             s.append(i)
         return len(set(s))==1
                 
