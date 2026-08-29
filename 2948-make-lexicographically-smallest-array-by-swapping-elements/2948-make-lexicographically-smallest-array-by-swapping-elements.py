58d67954518628882e5ac251c5f0e5a4cf97fe39
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-08-29 23:24:58 +0530

    Time: 386 ms (24.63%), Space: 58.4 MB (35.82%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,44 @@
+from typing import List
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        duplicate=nums[:]
+        nums.sort()
+        left = 0
+        right=1
+        current_array=[]
+        groups=[]
+        while right<len(nums):
+            if nums[right] - nums[left]<= limit:
+                if not current_array:
+                    current_array.append(nums[left])
+                current_array.append(nums[right])
+                left=right
+            else:
+                if current_array:
+                    groups.append(current_array)
+                current_array=[]
+                left=right
+            right+=1
+        if current_array:
+            groups.append(current_array)
+        positions = {}
+        for i in range(len(duplicate)):
+            if duplicate[i] not in positions:
+                positions[duplicate[i]] = []
+            positions[duplicate[i]].append(i)
+        for group in groups:
+            group_positions = []
+            used = {}
+            for value in group:
+                if value not in used:
+                    used[value] = 0
+                group_positions.append(positions[value][used[value]])
+                used[value] += 1
+            group_positions.sort()
+            group.sort()
+            for i in range(len(group)):
+                duplicate[group_positions[i]] = group[i]
+        return duplicate
+
+
+        
