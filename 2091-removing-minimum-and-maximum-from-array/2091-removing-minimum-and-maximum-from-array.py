f0309bd539dddfe6b5f620a24f5aefd5b68e066b
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-08-31 11:37:53 +0530

    Time: 27 ms (30.47%), Space: 34.1 MB (11.24%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,28 +1,14 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        count_max=0
-        count_min=0
         max_element=max(nums)
         min_element=min(nums)
-        n=[]
-        m=[]
-        for i in range(len(nums)):
-            if nums[i]!=max_element:
-                count_max+=1
-            else:
-                n.append(count_max+1)
-                count_max=0
-        n.append(count_max+1)
-        for i in range(len(nums)):
-            if nums[i]!=min_element:
-                count_min+=1
-            else:
-                m.append(count_min+1)
-                count_min=0
-        m.append(count_min+1)
-        both_left = max(n[0], m[0])
-        both_right = max(n[1], m[1])
-        left_right = n[0] + m[1]
-        right_left = n[1] + m[0]
-        return min(both_left, both_right, left_right, right_left)
+        max_index=nums.index(max_element)
+        min_index=nums.index(min_element)
+        left = min(max_index,min_index)
+        right = max(max_index,min_index)
+        n=len(nums)
+        both_left=right+1
+        both_right = n-left
+        left_right = left+1+n-right
+        return min(both_left, both_right, left_right)
         
