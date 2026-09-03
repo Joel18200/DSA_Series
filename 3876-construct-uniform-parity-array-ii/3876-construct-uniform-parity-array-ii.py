84301a9869292892b5384273ac4201df4c48da9c
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-09-03 23:35:55 +0530

    Time: 17 ms (83.54%), Space: 35.4 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        odd_parity=min(nums1)
-        if odd_parity%2!=0:
+        if min(nums1)%2!=0:
             return True
         for num in nums1:
             if num%2!=0:
