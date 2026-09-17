50c91a8a7d637fd9abfee018058033fc424b86f7
Joel Samuel <166829982+Joel18200@users.noreply.github.com>
2026-09-17 17:30:19 +0530

    Time: 55 ms (50.49%), Space: 31.3 MB (64.68%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -4,9 +4,9 @@ class Solution:
         asc=True
         desc=True
         for i in range(len(nums)-1):
-            if nums[i]<nums[i+1]:
-                asc=False
             if nums[i]>nums[i+1]:
+                asc=False
+            if nums[i]<nums[i+1]:
                 desc=False 
         return asc or desc                      
 
